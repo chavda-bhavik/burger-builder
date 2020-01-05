aaa3804a814b5782e18e86bb57700599e4d6e3e1
chavda-bhavik <bhavik.chvda@gmail.com>
2020-01-05 13:20:16 +0530

    added sideDrawer and made site mobile friendly

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,14 +3,28 @@ import React from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        
-        <main className={classes.content}>
-            {props.children}
-        </main>
-    </Aux>
-) 
+class layout extends React.Component {
+    state = {
+        showSideDrawer: false
+    }
+    SideDrawerToggledHandler = () => {
+        this.setState((state,props) => { 
+            return { showSideDrawer: !state.showSideDrawer } 
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar toggle={this.SideDrawerToggledHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerToggledHandler} />
+                <main className={classes.content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 export default layout
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import classes from './Toolbar.css'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from './DrawerToggle/DrawerToggle'
+
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
+        <DrawerToggle clicked={props.toggle}/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
