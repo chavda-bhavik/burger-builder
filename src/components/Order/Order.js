7f4bc4302dbd0890b4525d3892cb402dab416ff6
chavda-bhavik <bhavik.chvda@gmail.com>
2020-01-29 16:52:23 +0530

    Completed Simple Burger builder App 1

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,29 @@ import React from 'react'
 
 import classes from './Order.css'
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-)
+const order = (props) => {
+    const ingredients = []
+    for (let ingredient in props.ingredients) {
+        ingredients.push( { name:ingredient, amount: props.ingredients[ingredient] } )
+    }
+    console.log(ingredients)
+    const ingredientOutput = ingredients.map(ig => {
+        return <span
+                style={{ 
+                    textTransform:'capitalize',
+                    display:'inline-block',
+                    margin: '0 8px',
+                    border: 'px solid #css'
+
+                }}
+                 key={ig.name} >{ig.name} ({ig.amount})</span>
+    })
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)} </strong></p>
+        </div>
+    )
+}
 
 export default order
