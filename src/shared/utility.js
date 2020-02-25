c82fdace825c0dbbce9b03c784977e5df83462b4
Bhavik Chavda <bhavik.chvda@gmail.com>
2020-02-25 21:51:01 +0530

    outsourced(checkvalidity): outsourced and used checkvalidity function in app

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,22 @@ export const updateObject = (oldObject, updatedProperties) => {
         ...updatedProperties
     }
 }
+export const checkValidity = (value, rules) => {
+    let isValid = true
+    if(!rules) return true;
+    if(rules.required) {
+        isValid = value.trim() !== "" && isValid
+    }
+    if(rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid
+    }
+    if(rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid
+    }
+    if(rules.isEmail) {
+        // eslint-diable-next-line
+        const pattern = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/
+        isValid = pattern.test(value) && isValid
+    }
+    return isValid
+}
