024f2c9232b388b355f3a669ab06a3134a4bb9ae
chavda-bhavik <bhavik.chvda@gmail.com>
2020-04-01 22:19:18 +0530

    Updated(Layout.js): updated Layout.js with Hooks

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from "react-redux";
 
 import Aux from '../../hoc/Aux/Aux'
@@ -6,34 +6,32 @@ import classes from './Layout.css'
 import Toolbar from './../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from './../../components/Navigation/SideDrawer/SideDrawer'
 
-class layout extends React.Component {
-    state = {
-        showSideDrawer: false
+const layout = (props) => {
+    const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+    const SideDrawerCloseHandler = () => {
+        setSideDrawerOpen(false)
     }
-    SideDrawerToggledHandler = () => {
-        this.setState((state,props) => { 
-            return { showSideDrawer: !state.showSideDrawer } 
-        })
+    const SideDrawerToggledHandler = () => {
+        setSideDrawerOpen(!sideDrawerOpen)
     }
 
-    render() {
-        return (
-            <Aux>
-                <Toolbar 
-                    isAuth={this.props.isAuthenticated}
-                    toggle={this.SideDrawerToggledHandler}
-                />
-                <SideDrawer 
-                    isAuth={this.props.isAuthenticated}
-                    open={this.state.showSideDrawer} 
-                    closed={this.SideDrawerToggledHandler} 
-                />
-                <main className={classes.content}>
-                    {this.props.children}
-                </main>
-            </Aux>
-        )
-    }
+    return (
+        <Aux>
+            <Toolbar 
+                isAuth={props.isAuthenticated}
+                toggle={SideDrawerToggledHandler}
+            />
+            <SideDrawer 
+                isAuth={props.isAuthenticated}
+                open={sideDrawerOpen} 
+                closed={SideDrawerCloseHandler} 
+            />
+            <main className={classes.content}>
+                {props.children}
+            </main>
+        </Aux>
+    )
 }
 const mapStateToProps = state => {
     return {
